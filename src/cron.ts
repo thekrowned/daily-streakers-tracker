6f7fc20922e59d3f4e828785be8cd464d1af4155
Krowned <30321932-thekrowned@users.noreply.gitlab.com>
2025-09-26 12:57:29 +0800

    Add try catch on "cron" script

--- src/cron.ts
+++ src/cron.ts
@@ -10,91 +10,100 @@ const beginningDate = new Date("2024-07-25T00:00:00+00:00");
 async function getPlayersInfo() {
 	const logTime = new Date();
 	console.info("[getPlayersInfo]", logTime.toUTCString());
-	const playerNames = await readJson(
-		path.join(path.resolve(), "src", "tracked-players.json")
-	);
 
-	if (!Array.isArray(playerNames)) {
-		throw new Error("The JSON file should be in array");
-	}
+	const TIMEOUT = 1800_000;
+	const TIMEOUT_RETRY = 60_000;
+	let ERROR_STATUS = false;
 
-	for (let i = 0; i < playerNames.length; i++) {
-		const playerName = assertString(playerNames[i]);
-		console.info("[getPlayersInfo]", `Inserting ${playerName}`);
-
-		const user = await OsuAPI.getUser(playerName);
-
-		const lastUpdate = user.daily_challenge_user_stats.last_update;
-		const today = new Date();
-
-		const playedToday = (() => {
-			const lastDate = lastUpdate?.getUTCDate();
-			const lastMonth = lastUpdate?.getUTCMonth();
-			const lastYear = lastUpdate?.getUTCFullYear();
-
-			const todayDate = today?.getUTCDate();
-			const todayMonth = today?.getUTCMonth();
-			const todayYear = today?.getUTCFullYear();
-
-			if (
-				todayDate == lastDate &&
-				todayMonth == lastMonth &&
-				todayYear == lastYear
-			) {
-				return true;
-			} else {
-				return false;
-			}
-		})();
-
-		const playerInsertData = {
-			id: user.id,
-			name: user.username,
-			rank_standard: user.statistics.global_rank ?? 0,
-			current_streak: user.daily_challenge_user_stats.daily_streak_current,
-			has_played_today: playedToday,
-			total_participation: user.daily_challenge_user_stats.playcount,
-		};
-
-		const dbExistingPlayer = await DB.players.getById(user.id);
-		const existingPlayer = await dbExistingPlayer.getRowObjectsJson();
-
-		if (existingPlayer.length != 0) {
-			await DB.players.update(playerInsertData);
-		} else {
-			await DB.players.add(playerInsertData);
-		}
-
-		const millisecondsSinceBeginning =
-			today.getTime() - beginningDate.getTime();
-		const daysSinceBeginning = Math.floor(
-			millisecondsSinceBeginning / 86400000
+	try {
+		const playerNames = await readJson(
+			path.join(path.resolve(), "src", "tracked-players.json")
 		);
 
-		const streakerInsertData = {
-			id: user.id,
-			full_streaker:
-				user.daily_challenge_user_stats.daily_streak_current >=
-				daysSinceBeginning
-					? true
-					: false,
-			is_streaking:
-				user.daily_challenge_user_stats.daily_streak_current >= 2
-					? true
-					: false,
-		};
-
-		const dbExistingStreak = await DB.streaker_tracker.getById(user.id);
-		const existingStreak = await dbExistingStreak.getRowObjectsJson();
-
-		if (existingStreak.length != 0) {
-			await DB.streaker_tracker.update(streakerInsertData);
-		} else {
-			await DB.streaker_tracker.add(streakerInsertData);
+		if (!Array.isArray(playerNames)) {
+			throw new Error("The JSON file should be in array");
 		}
+		for (let i = 0; i < playerNames.length; i++) {
+			const playerName = assertString(playerNames[i]);
+			console.info("[getPlayersInfo]", `Inserting ${playerName}`);
+
+			const user = await OsuAPI.getUser(playerName);
+
+			const lastUpdate = user.daily_challenge_user_stats.last_update;
+			const today = new Date();
+
+			const playedToday = (() => {
+				const lastDate = lastUpdate?.getUTCDate();
+				const lastMonth = lastUpdate?.getUTCMonth();
+				const lastYear = lastUpdate?.getUTCFullYear();
+
+				const todayDate = today?.getUTCDate();
+				const todayMonth = today?.getUTCMonth();
+				const todayYear = today?.getUTCFullYear();
+
+				if (
+					todayDate == lastDate &&
+					todayMonth == lastMonth &&
+					todayYear == lastYear
+				) {
+					return true;
+				} else {
+					return false;
+				}
+			})();
+
+			const playerInsertData = {
+				id: user.id,
+				name: user.username,
+				rank_standard: user.statistics.global_rank ?? 0,
+				current_streak: user.daily_challenge_user_stats.daily_streak_current,
+				has_played_today: playedToday,
+				total_participation: user.daily_challenge_user_stats.playcount,
+			};
+
+			const dbExistingPlayer = await DB.players.getById(user.id);
+			const existingPlayer = await dbExistingPlayer.getRowObjectsJson();
+
+			if (existingPlayer.length != 0) {
+				await DB.players.update(playerInsertData);
+			} else {
+				await DB.players.add(playerInsertData);
+			}
+
+			const millisecondsSinceBeginning =
+				today.getTime() - beginningDate.getTime();
+			const daysSinceBeginning = Math.floor(
+				millisecondsSinceBeginning / 86400000
+			);
+
+			const streakerInsertData = {
+				id: user.id,
+				full_streaker:
+					user.daily_challenge_user_stats.daily_streak_current >=
+					daysSinceBeginning
+						? true
+						: false,
+				is_streaking:
+					user.daily_challenge_user_stats.daily_streak_current >= 2
+						? true
+						: false,
+			};
+
+			const dbExistingStreak = await DB.streaker_tracker.getById(user.id);
+			const existingStreak = await dbExistingStreak.getRowObjectsJson();
+
+			if (existingStreak.length != 0) {
+				await DB.streaker_tracker.update(streakerInsertData);
+			} else {
+				await DB.streaker_tracker.add(streakerInsertData);
+			}
+		}
+	} catch (error) {
+		console.error("[getPlayersInfo]", error);
+		ERROR_STATUS = true;
 	}
 
-	setTimeout(() => getPlayersInfo(), 1800_000);
+	setTimeout(() => getPlayersInfo(), ERROR_STATUS ? TIMEOUT_RETRY : TIMEOUT);
 }
 
 async function runCron() {
