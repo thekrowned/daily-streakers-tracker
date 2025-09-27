3c44fb5c8f33cc1115464a1b2de69ca565dfe896
Krowned <30321932-thekrowned@users.noreply.gitlab.com>
2025-09-27 11:37:01 +0800

    Add check storage availability function

---
+++ assets/storage-check.js
@@ -0,0 +1,19 @@
+// Stolen from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#feature-detecting_localstorage
+function storageAvailable(type) {
+	let storage;
+	try {
+		storage = window[type];
+		const x = "__storage_test__";
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	} catch (e) {
+		return (
+			e instanceof DOMException &&
+			e.name === "QuotaExceededError" &&
+			// acknowledge QuotaExceededError only if there's something already stored
+			storage &&
+			storage.length !== 0
+		);
+	}
+}
