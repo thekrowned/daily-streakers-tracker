69833155b73580e05c639894a1780f5ae66a3cbe
Krowned <151819685+thekrowned@users.noreply.github.com>
2025-10-14 15:12:38 +0800

    Remove DuckDB and its db functions

--- src/db/conn.ts
+++
@@ -1,7 +0,0 @@
-import { DuckDBInstance } from "@duckdb/node-api";
-
-const instance = await DuckDBInstance.create("storage.db");
-
-const storageConn = await instance.connect();
-
-export { storageConn };
--- src/db/migrate.ts
+++
@@ -1,3 +0,0 @@
-import { storageConn } from "./conn.js";
-
-await storageConn.run(`ALTER TABLE players ADD best_daily_streak integer;`);
