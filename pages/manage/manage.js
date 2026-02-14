b5c6c293e38be4b81ac6b1bb3c68ed85222e9631
Krowned <151819685+thekrowned@users.noreply.github.com>
2026-02-14 23:06:45 +0800

    Connect add players API with its related form UI

---
+++ pages/manage/manage.js
@@ -0,0 +1,51 @@
+const formAddPlayer = document.getElementById("form-add-player");
+
+formAddPlayer.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	const playerNames = [];
+
+	const data = new FormData(formAddPlayer);
+	const playerNameData = data.get("player-name");
+
+	if (typeof playerNameData == "string") {
+		const players = playerNameData.split(";");
+
+		for (let i = 0; i < players.length; i++) {
+			const player = players[i].trim();
+
+			if (player.length > 0) {
+				playerNames.push(player);
+			}
+		}
+	}
+
+	try {
+		if (playerNames.length <= 0) {
+			throw new Error("Empty or undetected usernames!");
+		}
+
+		const addPlayersResponse = await fetch(
+			"../api/manage/add-tracked-players",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					players: playerNames,
+				}),
+				headers: { "Content-Type": "application/json" },
+			},
+		);
+
+		const addPlayersResData = await addPlayersResponse.json();
+
+		if (addPlayersResData?.success === true) {
+			alert(
+				`${playerNames.length} players detected. ${addPlayersResData?.message}`,
+			);
+			formAddPlayer.reset();
+		}
+	} catch (error) {
+		console.error(error);
+		alert(`${error?.message || "Something unexpected happened."}`);
+	}
+});
