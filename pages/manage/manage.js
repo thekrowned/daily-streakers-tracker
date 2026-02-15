f198b46827ae5d70d5167d5e3f466241478569fc
Krowned <151819685+thekrowned@users.noreply.github.com>
2026-02-15 20:44:37 +0800

    Generate players-item

--- pages/manage/manage.js
+++ pages/manage/manage.js
@@ -53,3 +53,63 @@ formAddPlayer.addEventListener("submit", async (e) => {
 		alert(`${error?.message || "Something unexpected happened."}`);
 	}
 });
+
+// ===== Generate players-item =====
+const playerItemTemplate = document.importNode(
+	document.getElementById("template-players-item").content,
+	true,
+);
+
+function createPlayersItem({ id, name }) {
+	const newPlayerItem = playerItemTemplate.cloneNode(true);
+	const input = newPlayerItem.querySelector("input");
+	input.setAttribute("name", `player-${id}`);
+	input.setAttribute("id", `player-${id}`);
+	input.setAttribute("data-id", id);
+	input.setAttribute("data-name", name);
+	const label = newPlayerItem.querySelector("label");
+	label.setAttribute("for", `player-${id}`);
+	label.textContent = name;
+
+	return newPlayerItem;
+}
+
+async function generatePlayersItem() {
+	try {
+		const res = await fetch("../api/manage/tracked-players");
+
+		if (!res.ok || res.status != 200) {
+			throw new Error("Error fetching data");
+		}
+
+		const data = await res.json();
+		const players = await data.data.players;
+
+		if (!Array.isArray(players)) {
+			throw new Error("Data is not of array type");
+		}
+
+		const playersList = document.querySelector("ul.players-list");
+
+		playersList.innerHTML = "";
+
+		players.forEach((player) => {
+			const playerItem = createPlayersItem({
+				id: player.osu_id,
+				name: player.name,
+			});
+
+			playersList.appendChild(playerItem);
+		});
+	} catch (error) {
+		console.error(error);
+		alert(`${error?.message || "Something unexpected happened."}`);
+	}
+}
+
+// ===== Main =====
+async function main() {
+	await generatePlayersItem();
+}
+
+main();
