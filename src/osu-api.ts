24d24eb4aa53978b0059852915b230b277965edb
Krowned <151819685+thekrowned@users.noreply.github.com>
2026-03-20 22:34:46 +0800

    Disable internal automatic setNewToken - I have a suspicion that this routine was executed without error handling, hence crashing the app

--- src/osu-api.ts
+++ src/osu-api.ts
@@ -64,7 +64,8 @@ const OsuAPI = class {
 
 	static generateApi = async function () {
 		try {
-			const api = await API.createAsync(ownClientId, ownClientSecret);
+			consolePref.info(`Generating API...`);
+			const api = await API.createAsync(ownClientId, ownClientSecret, undefined, {set_token_on_expires: false, set_token_on_401: false});
 			OsuAPI.#api = api;
 		} catch (error) {
 			TimerManager.addTimeout({
