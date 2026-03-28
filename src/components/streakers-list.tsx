5ff0816fbf89b2930e61c09c26b60f33ab7212a4
The Krowned <151819685+thekrowned@users.noreply.github.com>
2026-03-28 19:40:38 +0800

    Add streakers list & item components

---
+++ src/components/streakers-list.tsx
@@ -0,0 +1,99 @@
+function StreakersList({ children }: { children?: any }) {
+	return <ul class="streakers-list">{children}</ul>;
+}
+
+function StreakersItem({
+	playerName,
+	osuId,
+	hasPlayedToday,
+	tierIndex,
+	currentStreak,
+	bestStreak,
+	previousStreak,
+	showCurrent,
+	showBest,
+}: {
+	playerName: string;
+	osuId: number;
+	hasPlayedToday: boolean;
+	tierIndex: number;
+	currentStreak: number;
+	bestStreak: number;
+	previousStreak: number;
+	showCurrent?: boolean;
+	showBest?: boolean;
+}) {
+	let arrowTierSrc: string = "";
+	let altTier: string = "";
+	let outlineTierClassname: string = "";
+
+	let showPrevious: boolean = false;
+
+	switch (tierIndex) {
+		case -3:
+			arrowTierSrc = "./assets/arrow-down-double.png";
+			altTier = "This player is no longer a full streaker.";
+			outlineTierClassname = "streakers-list__item--fallen";
+			showPrevious = true;
+			break;
+		case -2:
+			arrowTierSrc = "./assets/arrow-down-double.png";
+			altTier = "This player has just lost their streak significantly.";
+			outlineTierClassname = "streakers-list__item--downgraded";
+			showPrevious = true;
+			break;
+		case -1:
+			arrowTierSrc = "./assets/arrow-down.png";
+			altTier = "This player is no longer a casual streaker.";
+			showPrevious = true;
+			break;
+		case 1:
+			arrowTierSrc = "./assets/arrow-up.png";
+			altTier = "This player has just became a casual streaker.";
+			outlineTierClassname = "streakers-list__item--upgraded";
+		default:
+			break;
+	}
+
+	return (
+		<li
+			class={`streakers-list__item ${hasPlayedToday ? "streakers-list__item--played" : outlineTierClassname}`}
+		>
+			<a
+				class="streakers-list__link"
+				target="_blank"
+				href={`https://osu.ppy.sh/users/${osuId}`}
+				aria-description={hasPlayedToday ? "Played today" : ""}
+			>
+				{playerName}
+			</a>
+			{showCurrent && currentStreak > 0 && (
+				<div class="streakers-list__count" aria-description="Current Streak">
+					{showPrevious || showBest ? `Curr: ${currentStreak}` : currentStreak}
+				</div>
+			)}
+			{showBest && bestStreak > 0 && (
+				<div class="streakers-list__count" aria-description="Best Streak">
+					{showPrevious || showCurrent ? `Best: ${bestStreak}` : bestStreak}
+				</div>
+			)}
+			{showPrevious && (
+				<div class="streakers-list__count" aria-description="Previous Streak">
+					{`Prev: ${previousStreak}`}
+				</div>
+			)}
+			{/* <div class="streakers-list__insert-helper"></div> */}
+			{arrowTierSrc && (
+				<div class="streakers-list__tier">
+					<img
+						class="streakers-list__tier-image"
+						alt={altTier}
+						src={arrowTierSrc}
+					/>
+				</div>
+			)}
+		</li>
+	);
+}
+
+export { StreakersItem, StreakersList };
